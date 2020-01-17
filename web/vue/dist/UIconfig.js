0560ceaf49db9939b4767d6aebf48a20e4da4767
Igor <igor_paiva21@live.com>
2020-01-17 01:05:35 -0300

    Editando config

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -7,13 +7,13 @@
 const CONFIG = {
   headless: false,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
-    host: 'localhost',
+    host: '167.172.158.68 ',
     port: 3000,
     path: '/'
   },
